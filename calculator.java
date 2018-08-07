e7fe4cf2935661da8e390e9da3d35a9ddc9badce
sskimin4 <37132420+sskimin4@users.noreply.github.com>
2018-08-07 14:48:57 +0900

    sum()함수 부분 추가

--- calculator.java
+++ calculator.java
@@ -3,6 +3,12 @@ package com.mycompany.test;
 public class calculator{
 
   public static void main(String[] args){
+    int a=2;
+    int b=4;
 
+    System.out.println( sum(a,b));
+  }
+  private static int sum(int a,int b){
+    return a+b;
   }
 }
